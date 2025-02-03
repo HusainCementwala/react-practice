e8d77fefcc36aa60d6f1a0c25cec8d0e0199ce37
Husain <husaincementwalas@gmail.com>
2025-02-03 20:18:56 +0530

    currency convert complete

---
+++ currencyconvert/src/components/Index.js
@@ -0,0 +1,7 @@
+import InputBox from "./InputBox";
+
+export {InputBox}
+
+
+//this file is created to collec all components in one place and then export to ap.jsx collectively and use
+//used mainly in production
